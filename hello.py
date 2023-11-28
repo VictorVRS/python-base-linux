94363f93c4ab9fb32d3946c7727a16a51b9b459e
VictorVRS <victorrodrigosilva@gmail.com>
2023-11-28 11:33:39 -0300

    Atualizações do Programas

--- hello.py
+++ hello.py
@@ -15,24 +15,54 @@ Execução:
      ou
      ./hello.py
 """
-__version__= "0.0.1"
+__version__= "0.1.3"
 __author__ = "Victor Rodrigo"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
-msg = "Hello, World!"
+arguments = {
+    "lang": None,
+    "count": 1,
+}
+for arg in sys.argv[1:]:
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option `{key}`")
+        sys.exit()
+    arguments[key] = value    
+    
 
-if current_language == "pt_BR":
-    msg = "Olá, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mondo!"    
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "fr_FR":
-    msg = "Bonjour, Monde!"    
+current_language = arguments["lang"]
+if current_language is None:
+    if "LANG" in os.environ:
+        current_language = os.getenv("LANG")
+    else: 
+        current_language = input("Choose a language:")
+
+
+current_language = current_language[:5]    
+    
+msg = {
+    "en_US":"Hello, World!",
+    "pt_BR":"Olá,Mundo!",
+    "it_IT":"Ciao,Mondo!",
+    "es_SP":"Hola, Mundo!",
+    "fr_FR":"Bonjour, Monde!",}
+
+print(msg[current_language] * int(arguments["count"]))        
+
+#if current_language == "pt_BR":
+   # msg = "Olá, Mundo!"
+#elif current_language == "it_IT":
+   # msg = "Ciao, Mondo!"    
+#elif current_language == "es_SP":
+   # msg = "Hola, Mundo!"
+#elif current_language == "fr_FR":
+   # msg = "Bonjour, Monde!"    
 
 
 
-print(msg)
